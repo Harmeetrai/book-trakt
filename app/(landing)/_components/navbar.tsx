8c28055150eeea70ffa0f11470147a3e02d5abdb
Harmeetrai <harmeetrai18@gmail.com>
2024-05-30 13:55:08 -0700

    add landing page componenets

---
+++ app/(landing)/_components/navbar.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import React from "react";
+import { useScrollTop } from "@/hooks/use-scroll-top";
+import { cn } from "@/lib/utils";
+import { Logo } from "./logo";
+import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, SignUpButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Spinner } from "@/components/spinner";
+export const Navbar = () => {
+    const scrolled = useScrollTop();
+    const { isAuthenticated, isLoading } = useConvexAuth();
+    return (
+        <div className={cn(
+            "z-50 bg-background dark:bg-[#1f1f1f] fixed top-0 flex items-center w-full p-6",
+            scrolled ? "shadow-md" : "shadow-none"
+        )}>
+            <Logo/>
+            <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+            {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Login
+              </Button>
+            </SignInButton>
+            <SignUpButton mode="modal">
+              <Button size="sm">Get BookTrakt free</Button>
+            </SignUpButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/tasks">Enter BookTrakt</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
+
+        <ModeToggle />
+            </div>
+        </div>
+
+    );
+}
